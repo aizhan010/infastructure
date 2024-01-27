c2da23383c9d031c2ea71a5bf0495041a1757aa2
aizhan010 <148381335+aizhan010@users.noreply.github.com>
2024-01-27 12:02:17 -0600

    added data

--- ec2/main.tf
+++ ec2/main.tf
@@ -24,4 +24,15 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+
 
